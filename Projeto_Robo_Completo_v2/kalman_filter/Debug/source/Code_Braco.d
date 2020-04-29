b71ee2e13fc66f5deb05ea1464af79d901356a74
Davi Alexandre <davialexandre_dap@hotmail.com>
2020-04-29 18:07:59 -0300

    fix:Filtro Kalman

---
+++ Projeto_Robo_Completo_v2/kalman_filter/Debug/source/Code_Braco.d
@@ -0,0 +1,81 @@
+source/Code_Braco.o source/Code_Braco.d: ../source/Code_Braco.c \
+ C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\source/mpu_6050.h \
+ C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\drivers/fsl_i2c.h \
+ C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\drivers/fsl_common.h \
+ C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\CMSIS/fsl_device_registers.h \
+ C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\CMSIS/MKL25Z4.h \
+ C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\CMSIS/core_cm0plus.h \
+ C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\CMSIS/core_cmInstr.h \
+ C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\CMSIS/cmsis_gcc.h \
+ C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\CMSIS/core_cmFunc.h \
+ C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\CMSIS/system_MKL25Z4.h \
+ C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\CMSIS/MKL25Z4_features.h \
+ C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\drivers/fsl_clock.h \
+ C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\board/board.h \
+ C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\board/clock_config.h \
+ C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\drivers/fsl_common.h \
+ C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\drivers/fsl_gpio.h \
+ C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\board/peripherals.h \
+ C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\board/pin_mux.h \
+ C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\board/clock_config.h \
+ C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\CMSIS/MKL25Z4.h \
+ C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\utilities/fsl_debug_console.h \
+ ../source/function.h ../source/perifericos.h \
+ C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\drivers/fsl_dac.h \
+ C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\drivers/fsl_pit.h \
+ C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\drivers/fsl_tpm.h \
+ ../source/Kalman.h
+
+C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\source/mpu_6050.h:
+
+C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\drivers/fsl_i2c.h:
+
+C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\drivers/fsl_common.h:
+
+C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\CMSIS/fsl_device_registers.h:
+
+C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\CMSIS/MKL25Z4.h:
+
+C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\CMSIS/core_cm0plus.h:
+
+C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\CMSIS/core_cmInstr.h:
+
+C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\CMSIS/cmsis_gcc.h:
+
+C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\CMSIS/core_cmFunc.h:
+
+C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\CMSIS/system_MKL25Z4.h:
+
+C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\CMSIS/MKL25Z4_features.h:
+
+C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\drivers/fsl_clock.h:
+
+C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\board/board.h:
+
+C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\board/clock_config.h:
+
+C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\drivers/fsl_common.h:
+
+C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\drivers/fsl_gpio.h:
+
+C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\board/peripherals.h:
+
+C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\board/pin_mux.h:
+
+C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\board/clock_config.h:
+
+C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\CMSIS/MKL25Z4.h:
+
+C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\utilities/fsl_debug_console.h:
+
+../source/function.h:
+
+../source/perifericos.h:
+
+C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\drivers/fsl_dac.h:
+
+C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\drivers/fsl_pit.h:
+
+C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Projeto_Robo_Completo_v1\kalman_filter\drivers/fsl_tpm.h:
+
+../source/Kalman.h:
---
+++ Projeto_Robo_Completo_v2/kalman_filter/source/kalman_filter_elo_todos/Debug/drivers/fsl_i2c.d
@@ -0,0 +1,33 @@
+drivers/fsl_i2c.o drivers/fsl_i2c.d: ../drivers/fsl_i2c.c \
+ ../drivers/fsl_i2c.h ../drivers/fsl_common.h \
+ C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Identi_Elos\Ident_bias\CMSIS/fsl_device_registers.h \
+ C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Identi_Elos\Ident_bias\CMSIS/MKL25Z4.h \
+ C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Identi_Elos\Ident_bias\CMSIS/core_cm0plus.h \
+ C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Identi_Elos\Ident_bias\CMSIS/core_cmInstr.h \
+ C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Identi_Elos\Ident_bias\CMSIS/cmsis_gcc.h \
+ C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Identi_Elos\Ident_bias\CMSIS/core_cmFunc.h \
+ C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Identi_Elos\Ident_bias\CMSIS/system_MKL25Z4.h \
+ C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Identi_Elos\Ident_bias\CMSIS/MKL25Z4_features.h \
+ ../drivers/fsl_clock.h
+
+../drivers/fsl_i2c.h:
+
+../drivers/fsl_common.h:
+
+C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Identi_Elos\Ident_bias\CMSIS/fsl_device_registers.h:
+
+C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Identi_Elos\Ident_bias\CMSIS/MKL25Z4.h:
+
+C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Identi_Elos\Ident_bias\CMSIS/core_cm0plus.h:
+
+C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Identi_Elos\Ident_bias\CMSIS/core_cmInstr.h:
+
+C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Identi_Elos\Ident_bias\CMSIS/cmsis_gcc.h:
+
+C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Identi_Elos\Ident_bias\CMSIS/core_cmFunc.h:
+
+C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Identi_Elos\Ident_bias\CMSIS/system_MKL25Z4.h:
+
+C:\Users\davia\OneDrive\Documentos\Mestrado_Softwares\Braco_Codigos\Identi_Elos\Ident_bias\CMSIS/MKL25Z4_features.h:
+
+../drivers/fsl_clock.h:
